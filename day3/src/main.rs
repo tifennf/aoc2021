dea0ac2cf48aaf912a7dcb21b77fbb70eb15b15b
tifenn <tifenn.fl@gmail.com>
2021-12-03 23:23:21 +0100

    ex2 day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let inputs = fs::read_to_string("./inputs.txt").unwrap();
 
     let gamma_rate = gamma_rate(&inputs);
-    let espilon_rate = espsilon_rate(&gamma_rate);
+    let espilon_rate = EpsilonRate(reverse_bits(&gamma_rate.0));
 
     let gamma_rate = binary_to_decimal(&gamma_rate.0);
     let espilon_rate = binary_to_decimal(&espilon_rate.0);
@@ -18,6 +18,19 @@ fn main() {
         "result: \n{}\n{}\nPOWER: {}",
         gamma_rate, espilon_rate, power_cons
     );
+    let bytes_list: Vec<_> = inputs.lines().map(|s| s.as_bytes()).collect();
+
+    let mut i = 0;
+    let oxy = aaaaaaaaaaaaa(&bytes_list, &mut i);
+    let mut j = 0;
+    let co2 = bbbbbbbbbbbb(&bytes_list, &mut j);
+
+    let oxy = binary_to_decimal(&oxy);
+    let co2 = binary_to_decimal(&co2);
+
+    let result = oxy * co2;
+
+    println!("oxy: {}\nco2: {}\nresult: {}", oxy, &co2, result);
 }
 
 struct GammaRate(String);
@@ -40,15 +53,11 @@ fn binary_to_decimal(n: &str) -> u32 {
     result as u32
 }
 
-fn espsilon_rate(gamma: &GammaRate) -> EpsilonRate {
-    let bytes: String = gamma
-        .0
+fn reverse_bits(bytes: &str) -> String {
+    bytes
         .chars()
         .map(|bit| if bit == '0' { '1' } else { '0' })
-        .collect();
-
-    // u32::from_str_radix(&bytes, 10).unwrap();
-    EpsilonRate(bytes)
+        .collect()
 }
 
 fn gamma_rate(inputs: &str) -> GammaRate {
@@ -78,4 +87,75 @@ fn gamma_rate(inputs: &str) -> GammaRate {
     }
 
     GammaRate(bit_count.iter().map(|bit| bit.to_string()).collect())
+}
+
+fn aaaaaaaaaaaaa(inputs: &Vec<&[u8]>, i: &mut usize) -> String {
+    if inputs.len() < 2 {
+        let x = inputs.iter().next().unwrap();
+
+        x.iter()
+            .map(|bit| {
+                if *bit == ZERO {
+                    0.to_string()
+                } else {
+                    1.to_string()
+                }
+            })
+            .collect()
+    } else {
+        let mut zero_list = Vec::new();
+        let mut one_list = Vec::new();
+
+        for bytes in inputs.iter() {
+            let bit = bytes[*i];
+
+            if bit == ZERO {
+                zero_list.push(*bytes);
+            } else {
+                one_list.push(*bytes);
+            }
+        }
+
+        *i += 1;
+        if one_list.len() >= zero_list.len() {
+            aaaaaaaaaaaaa(&one_list, i)
+        } else {
+            aaaaaaaaaaaaa(&zero_list, i)
+        }
+    }
+}
+fn bbbbbbbbbbbb(inputs: &Vec<&[u8]>, i: &mut usize) -> String {
+    if inputs.len() < 2 {
+        let x = inputs.iter().next().unwrap();
+
+        x.iter()
+            .map(|bit| {
+                if *bit == ZERO {
+                    0.to_string()
+                } else {
+                    1.to_string()
+                }
+            })
+            .collect()
+    } else {
+        let mut zero_list = Vec::new();
+        let mut one_list = Vec::new();
+
+        for bytes in inputs.iter() {
+            let bit = bytes[*i];
+
+            if bit == ZERO {
+                zero_list.push(*bytes);
+            } else {
+                one_list.push(*bytes);
+            }
+        }
+
+        *i += 1;
+        if one_list.len() < zero_list.len() {
+            bbbbbbbbbbbb(&one_list, i)
+        } else {
+            bbbbbbbbbbbb(&zero_list, i)
+        }
+    }
 }
